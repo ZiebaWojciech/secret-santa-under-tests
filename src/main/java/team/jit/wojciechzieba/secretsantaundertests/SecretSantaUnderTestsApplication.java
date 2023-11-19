a1e5fe8eda18c7c921c54682f1251dc11cd84b4f
Wojciech Zięba <wojciech.zieba@allegro.pl>
2023-11-19 12:49:39 +0100

    SEC-1 | init

---
+++ src/main/java/team/jit/wojciechzieba/secretsantaundertests/SecretSantaUnderTestsApplication.java
@@ -0,0 +1,13 @@
+package team.jit.wojciechzieba.secretsantaundertests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaUnderTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretSantaUnderTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/jit/wojciechzieba/secretsantaundertests/SecretSantaUnderTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package team.jit.wojciechzieba.secretsantaundertests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaUnderTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
