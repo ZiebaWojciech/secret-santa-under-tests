a9e972d029d9481a0f92abda453479818734d5b6
Wojtek <4630361+ZiebaWojciech@users.noreply.github.com>
2023-11-27 08:45:59 +0100

    SEC-3 | test containers - annotation based phase

--- src/test/java/team/jit/wojciechzieba/secretsantaundertests/IntegrationTest.java
+++ src/test/java/team/jit/wojciechzieba/secretsantaundertests/IntegrationTest.java
@@ -1,32 +1,38 @@
 package team.jit.wojciechzieba.secretsantaundertests;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import static java.lang.StringTemplate.STR;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@Slf4j
 class IntegrationTest {
 
     @Autowired
     WebTestClient webTestClient;
 
-    MongoDBContainer mongoDBContainer;
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"))
+            .withExposedPorts();
+
 
     @Test
     @DisplayName("Should create new Participant, when the creation request is valid")
     void shouldCreateNewParticipant_whenValidRequest() {
         // given
-
-        mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
-        mongoDBContainer.addExposedPort(20737);
-        mongoDBContainer.start();
 
         ParticipantDto participantDto = new ParticipantDto(
                 null,
@@ -40,16 +46,24 @@ class IntegrationTest {
         webTestClient.post()
                 .uri("/participants")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(STR."""
+                .bodyValue(STR. """
                             {
-                                "name": "\{participantDto.name()}",
-                                "hobby": "\{participantDto.hobby()}",
-                                "favouriteColour": "\{participantDto.favouriteColour()}"
+                                "name": "\{ participantDto.name() }",
+                                "hobby": "\{ participantDto.hobby() }",
+                                "favouriteColour": "\{ participantDto.favouriteColour() }"
                             }
-                        """)
+                        """ )
                 .exchange()
                 .expectStatus()
                 .isOk();
 
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"First", "Second"})
+    void parametrizedTestContainerTest(String param) {
+        log.warn(
+                STR. "\{param} test \{ mongoDBContainer.getContainerId() }"
+        );
     }
 }
