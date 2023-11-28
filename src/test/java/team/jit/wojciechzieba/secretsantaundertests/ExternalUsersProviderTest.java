43447374c8671d85faf8a4cba26ad387f526214b
Wojtek <4630361+ZiebaWojciech@users.noreply.github.com>
2023-11-28 15:15:40 +0100

    SEC-4 |  wiremock test

--- src/test/java/team/jit/wojciechzieba/secretsantaundertests/ExternalUsersProviderTest.java
+++ src/test/java/team/jit/wojciechzieba/secretsantaundertests/ExternalUsersProviderTest.java
@@ -2,6 +2,7 @@ package team.jit.wojciechzieba.secretsantaundertests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -10,9 +11,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
-@ContextConfiguration(classes = ExternalUsersSourceConfiguration.class )
+@ContextConfiguration(classes = ExternalUsersSourceConfiguration.class)
+@WireMockTest
 class ExternalUsersProviderTest {
 
     @Autowired
@@ -24,13 +30,18 @@ class ExternalUsersProviderTest {
     @Test
     void shouldRegisterGift_whenExternalUserProviderUsed() throws JsonProcessingException {
         // given
-
         GiftRegistrationCommand giftRegistrationCommand = new GiftRegistrationCommand(
                 "Maciej",
                 "czarny",
                 "dodatkowe zadanie",
                 "nadgodziny"
         );
+
+        stubFor(get("/users").willReturn(
+                aResponse()
+                        .withStatus(200)
+                        .withBody(UsersTestFactory.USERS_JSON)
+        ));
 
         // when
         webTestClient.post()
---
+++ src/test/java/team/jit/wojciechzieba/secretsantaundertests/UsersTestFactory.java
@@ -0,0 +1,40 @@
+package team.jit.wojciechzieba.secretsantaundertests;
+
+class UsersTestFactory {
+
+    public static final String USERS_JSON = """
+            [
+                {
+                    "name": "Piotr",
+                    "hobby": "zdrada aka samorozwój",
+                    "favouriteColour": "biały"
+                },
+                {
+                    "name": "Wojtek",
+                    "hobby": "imprezy integracyjne",
+                    "favouriteColour": "niebieski"
+                },
+                {
+                    "name": "Matylda",
+                    "hobby": "ass-kicking",
+                    "favouriteColour": "czarny"
+                },
+                {
+                    "name": "Maciej",
+                    "hobby": "absynt",
+                    "favouriteColour": "zielony"
+                },
+                {
+                    "name": "Domżi",
+                    "hobby": "klocki lego",
+                    "favouriteColour": "piwny"
+                },
+                {
+                    "name": "Muody",
+                    "hobby": "praca po godzinach",
+                    "favouriteColour": "tani"
+                }
+            ]
+            """;
+
+}
